4d9dd89a61c9e35936669fe2b44a3cc66ed2a6a6
hunmatu <hunmatu@outlook.com>
2019-01-11 15:12:13 +0900

    画像の著作権面を考慮

--- main.py
+++ main.py
@@ -2,39 +2,49 @@
 import urllib.request
 import re
 import os
-import ctypes 
+import ctypes
 import pythoncom
 from win32com.shell import shell, shellcon
 import win32gui
 
-# url指定
-url ="https://www.bing.com" + re.findall("/az.*?.jpg",re.findall("g_img=.*?\".*?\"",str(urllib.request.urlopen("https://www.bing.com").read()))[0])[0]
+# html取得 type:str
+html = str(urllib.request.urlopen("https://www.bing.com").read())
 
-# 保存名の指定
-img = url.lstrip("https://www.bing.com/az/hprichbg/rb/")
+# 各種データ取得
+available = re.findall(                                                 # 利用可能か
+    "[^\":,\\s]+", re.findall("\"wp\":.*?,", html)[0])[1]
+
+if available == "false":                                                # 壁紙使用不可の画像の場合、ここでプログラム終了
+    print("can't download!")
+    exit()
+
+filename = re.findall(                                                  # 画像名
+    "[^=\"\\s]+", re.findall("imgName.*?\".*?\"", html)[0])[1] + ".jpg"
+
+url = "https://www.bing.com/hpwp/" + re.findall(                        # URL
+    "[^\":\\s]+", re.findall("\"hsh\":\".*?\"", html)[0])[1]
 
 # 保存先フォルダー作成
-if os.path.isdir('./picture') == False:
-    os.mkdir('./picture')
+if os.path.isdir('./pictures') == False:
+    os.mkdir('./pictures')
 
 # ダウンロード
-urllib.request.urlretrieve(url, "./picture/" + img)
+urllib.request.urlretrieve(url, "./pictures/" + filename)
 
 # 壁紙のパス
-path = str(os.path.dirname(os.path.abspath(__file__)))+"\\picture\\" + img
+path = str(os.path.dirname(os.path.abspath(__file__))) + \
+    "\\pictures\\" + filename
 
 # progman 再起動
-win32gui.SendMessageTimeout(win32gui.FindWindow('Progman', None),0x52c, None, None, 0, 1000)
+win32gui.SendMessageTimeout(win32gui.FindWindow(
+    'Progman', None), 0x52c, None, None, 0, 1000)
 
 # 壁紙の変更 1
-# SPI_SETDESKWALLPAPER = 20 
-# ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path , 0) 
+# SPI_SETDESKWALLPAPER = 20
+# ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path , 0)
 
 # 壁紙の変更 2
 iad = pythoncom.CoCreateInstance(shell.CLSID_ActiveDesktop, None,
-          pythoncom.CLSCTX_INPROC_SERVER, shell.IID_IActiveDesktop)
+                                 pythoncom.CLSCTX_INPROC_SERVER, shell.IID_IActiveDesktop)
 iad.SetWallpaper(path, 0)
 iad.ApplyChanges(shellcon.AD_APPLY_ALL)
-
-
-
